8f56ddec109a0f06c1eb889f3a79388ff5002a23
Tsukasa Yoneda <yone2ks@TsukasanoMacBook-Pro.local>
2024-10-07 06:54:34 +0900

    change dir structure and add client

---
+++ neteye_client/node/client.py
@@ -0,0 +1,39 @@
+from neteye_client.neteye_client import NeteyeClient
+from neteye_client.node.node import Node
+
+
+class Client:
+    def __init__(self, client: NeteyeClient):
+        self.client = client
+
+    def get_node(self, node_id: str) -> Node:
+        """
+        Retrieves a Node object with the specified ID.
+        """
+        path = f"{Node.NODE_PATH}/{node_id}"
+        node_data = self.client.get(path)
+        return Node.from_dict(node_data)
+
+    def create_node(self, node: Node) -> Node:
+        """
+        Creates a new Node object.
+        """
+        node_data = node.to_dict()
+        created_node_data = self.client.post(Node.NODE_PATH, data=node_data)
+        return Node.from_dict(created_node_data)
+
+    def update_node(self, node: Node) -> Node:
+        """
+        Updates an existing Node object.
+        """
+        path = f"{Node.NODE_PATH}/{node.id}"
+        node_data = node.to_dict()
+        updated_node_data = self.client.put(path, data=node_data)
+        return Node.from_dict(updated_node_data)
+
+    def delete_node(self, node_id: str) -> None:
+        """
+        Deletes a Node object with the specified ID.
+        """
+        path = f"{Node.NODE_PATH}/{node_id}"
+        self.client.delete(path)
