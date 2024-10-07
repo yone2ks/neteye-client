5c61a211f6efd41b67f58768b8fa168cde1db0fc
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2024-10-08 08:20:00 +0900

    delete verbose words

--- neteye_client/node/client.py
+++ neteye_client/node/client.py
@@ -6,32 +6,32 @@ class Client:
     def __init__(self, client: NeteyeClient):
         self.client = client
 
-    def get_node(self, node_id: str) -> Node:
+    def get(self, node_id: str) -> Node:
         """
         Retrieves a Node object with the specified ID.
         """
         path = f"{Node.NODE_PATH}/{node_id}"
-        node_data = self.client.get(path)
+        data = self.client.get(path)
         return Node.from_dict(node_data)
 
-    def create_node(self, node: Node) -> Node:
+    def create(self, node: Node) -> Node:
         """
         Creates a new Node object.
         """
-        node_data = node.to_dict()
+        data = node.to_dict()
         created_node_data = self.client.post(Node.NODE_PATH, data=node_data)
         return Node.from_dict(created_node_data)
 
-    def update_node(self, node: Node) -> Node:
+    def update(self, node: Node) -> Node:
         """
         Updates an existing Node object.
         """
         path = f"{Node.NODE_PATH}/{node.id}"
-        node_data = node.to_dict()
-        updated_node_data = self.client.put(path, data=node_data)
+        data = node.to_dict()
+        updated_data = self.client.put(path, data=node_data)
         return Node.from_dict(updated_node_data)
 
-    def delete_node(self, node_id: str) -> None:
+    def delete(self, node_id: str) -> None:
         """
         Deletes a Node object with the specified ID.
         """
